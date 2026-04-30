c36c1824fd8babec4882f68d39c031303cb4336d
gpont12 <74205329+gpont12@users.noreply.github.com>
2026-04-29 22:16:13 -0600

    Fixed import path for useAuth from '../auth' to './auth' in login page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { useAuth } from "../auth";
+import { useAuth } from "./auth";
 
 export default function LoginPage() {
   const [password, setPassword] = useState("");
